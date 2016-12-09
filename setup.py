53c708eba35ec0f9915e3103bbfc174455461b4c
ntz <netzuleando@gmail.com>
2016-12-09 21:17:58 +0100

    v0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='ntz-py',
     packages=['ntz-py'],
-    version='0.3',
+    version='0.4',
     description='Ntz own libraries for python',
     author='Netzulo.com',
     author_email='netzuleando@gmail.com',
     url='https://github.com/netzulo/ntz-py',
-    download_url='https://github.com/netzulo/ntz-py/tarball/0.1',
+    download_url='https://github.com/netzulo/ntz-py/tarball/0.4',
     keywords=['testing', 'integration', 'qa', 'functional', 'regresion'],
     classifiers=[],
 )
